2e667ef1401df886700bf8e46ad7da22c62213ac
jinbo-0523 <65935770+jinbo-0523@users.noreply.github.com>
2021-05-20 00:11:25 +0900

    レンダリング確認

---
+++ src/ChildArea.jsx
@@ -0,0 +1,17 @@
+export const ChildArea = (props) => {
+  const { open } = props;
+  const style = {
+    width: "100%",
+    height: "200px",
+    backgroundColor: "khaki"
+  };
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+        </div>
+      ) : null}
+    </>
+  );
+};
